0e33b141bf0875a77a6576beb2fccc44dde9ba13
agon-halili <ah41416@ubt-uni.net>
2023-05-01 17:51:37 +0200

    Created center component to center login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,28 +1,35 @@
-import { Box, Button, TextField } from '@mui/material';
+import { Box, Button, Card, CardContent, TextField } from '@mui/material';
 import React from 'react';
+import Center from './Center';
 
 export default function Login() {
   return (
-    <Box
-      sx={{
-        '& .MuiTextField-root': {
-          margin: 1,
-          width: '90%',
-        },
-      }}
-    >
-      <form noValidate>
-        <TextField label='Email' name='email' variant='outlined' />
-        <TextField label='Name' name='name' variant='outlined' />
-        <Button
-          type='submit'
-          variant='contained'
-          size='large'
-          sx={{ width: '90%' }}
-        >
-          Start
-        </Button>
-      </form>
-    </Box>
+    <Center>
+      <Card sx={{ width: 400 }}>
+        <CardContent>
+          <Box
+            sx={{
+              '& .MuiTextField-root': {
+                margin: 1,
+                width: '90%',
+              },
+            }}
+          >
+            <form noValidate>
+              <TextField label='Email' name='email' variant='outlined' />
+              <TextField label='Name' name='name' variant='outlined' />
+              <Button
+                type='submit'
+                variant='contained'
+                size='large'
+                sx={{ width: '90%' }}
+              >
+                Start
+              </Button>
+            </form>
+          </Box>
+        </CardContent>
+      </Card>
+    </Center>
   );
 }
