947fc8b163996b509e0663da926a1a2f6c6e2d69
arbenm9 <arbenm9@gmail.com>
2023-05-09 21:49:13 +0200

    implemented React router

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,18 @@
 import Login from './components/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Quiz from './components/Quiz';
+import Result from './components/Result';
 
 function App() {
-  return <Login />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/quiz' element={<Quiz />} />
+        <Route path='/result' element={<Result />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
